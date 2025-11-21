989901e3e297a9f332ac937c022817374ca1b658
MoeSalah1999 <salahkhw15@gmail.com>
2025-11-21 03:05:24 +0300

    Forgot to register the signals files in apps.py

--- Rakmedia/api/apps.py
+++ Rakmedia/api/apps.py
@@ -5,6 +5,7 @@ class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
 
+    # Include here whatever separate signals file you add to the application and want to utilize.
     def ready(self):
         import api.signals
         import api.utils.cache_signals
