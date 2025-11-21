31033eebcad79e3dd73b4d2db9f03bea63047664
MoeSalah1999 <salahkhw15@gmail.com>
2025-11-21 03:03:01 +0300

    Modified the EmployeeFilter in filters.py

--- Rakmedia/api/filters.py
+++ Rakmedia/api/filters.py
@@ -2,9 +2,12 @@ import django_filters
 from api.models import Employee
 
 
+
 class EmployeeFilter(django_filters.FilterSet):
     class Meta:
         model = Employee
+
+        # Add or remove whatever fields you want your filter to have or not have.
         fields = {
             'first_name': ['iexact', 'icontains'],
             'last_name': ['iexact', 'icontains'],
